934fc996d6446497973696534c14d7bd2f3fda2c
Griffohn <49243231+Griffohn@users.noreply.github.com>
2019-04-03 08:30:53 -0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,19 @@
+import java.util.*;
+    public class Game{
+    public static void main(String [] args){
+
+    Scanner input = new Scanner(System.in);
+    String User = "";
+    
+    System.out.println("Please tell me your FULL name...");
+    String User = input.nextLine()-2;
+    
+    int num = user.length();
+    
+    system.out.println("Hello, " User +" your name has " +num+ " letters.");
+    
+        
+    }
+    
+        
+}
